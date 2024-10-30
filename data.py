344e1dde52d69f7cae40d24a8a26fe36d1e657f5
Simon Hofinger <simon@hofinger.re>
2024-10-31 00:13:34 +0100

    added annotations and comments

--- data.py
+++ data.py
@@ -10,14 +10,15 @@ FIGSIZE = (4,3)
 
 
     
-# converts input string to normalized 2dim numpy float array
-def conv_data(data):
+# Converts an input string to a 2d np.array
+def conv_data(data: str) -> np.ndarray:
     horizontal = data.split('\n')
     for i,e in enumerate(horizontal):
         horizontal[i] = e.split(' ')[:-1]
     return np.array(horizontal[:-1],dtype=float)
 
-def normalize(data):
+# Normalizes a 2d np.array
+def normalize(data: np.ndarray) -> np.ndarray:
     result = np.empty((len(data),len(data[0])))
     maximum = 0
     for i in data:
@@ -29,59 +30,26 @@ def normalize(data):
             result[i][j] = f/maximum
     return result
 
-#top and bottom set the location on the
-def data_img_printer(data,name,top,bottom,pos,scale):
-    if not os.path.exists(os.getcwd() + 'img'):
-        os.makedirs(os.getcwd() + 'img')
-    fig, ax = plt.subplots(figsize=FIGSIZE)
-    im = ax.imshow(normalize(data[name][top:bottom]), interpolation='nearest')
-    ax.set_xticks(np.arange(len(data[name][0]))[::140] ,labels=axis(len(data[name][0]),pos,scale,True)[::140])
-    ax.set_title(name)
-    fig.tight_layout()
-    fig.savefig('img/' + name + '_'+ str(top) + '_' + str(bottom) + '_raw.png')
-
-def data_img_printer_full(data,name,pos,scale):
-    if not os.path.exists(os.getcwd() + 'img'):
-        os.makedirs(os.getcwd() + 'img')
-    fig, ax = plt.subplots(figsize=FIGSIZE)
-    im = ax.imshow(normalize(data[name]), interpolation='nearest')
-    ax.set_xticks(np.arange(len(data[name][0]))[::140] ,labels = axis(len(data[name][0]),pos,scale,True)[::140])
-    ax.set_title(name)
-    fig.tight_layout()
-    fig.savefig('img/' + name + '_full_raw.png')
-
-
-
-       
-#converts the data to a 1 dim line
-def compress(data,name,top,bottom):
+#converts the data to a 1 dim line (histogram with cut range)
+def compress(data: dict,name: str,top: int,bottom: int) -> np.ndarray:
     result = np.zeros(len(data[name][0]))
     for i in data[name][top:bottom]:
         for j,e in enumerate(i):
             result[j] = e + result[j]
     return result
 
-# scales a 1 Dim array by calculating the x average in iteratives steps and repositions the array to that average
-def norm(array, cut):
+# scales a 1 Dim array by cutting the top and bottom x (1,0) values, and shifting the values towards the resulting average
+def norm(array: np.ndarray, cut: float):
     vals = np.sort(array)
     cut = int(len(array)*cut)
     average = np.average(vals[cut:-cut])
     for i,e in enumerate(array):
         array[i] = e - average
     return array
-        
-def histogram(data,name,top,bottom,pos,scale):
-    if not os.path.exists(os.getcwd() + 'hist'):
-        os.makedirs(os.getcwd() + 'hist')
-    vals = norm(compress(data,name,top,bottom),0.1)
-    fig, ax = plt.subplots(figsize=FIGSIZE)
-    ax.plot(axis(len(vals),pos,scale,False),vals)
-    ax.set_title(name)
-    fig.tight_layout()
-    fig.savefig('hist/' + name + '_'+ str(top) + '_' + str(bottom) +'_hist.png')
-    plt.close()
-       
-def axis(len, pos, scale, rounding):
+
+#Generate a the Axis in nm, len is the total number of y values, pos is the wavelength of the center entry in nm, scale is the nm/mm factor of each grate in relation to the detector
+#Rounding enables rounding of resulting values (is used when using it as label for x axis, reduce count with [::140])
+def axis(len: int, pos: float, scale: float, rounding: bool):
     axis = np.zeros(len)
     begining = pos - round(len/2) * (scale * PIXELSIZE * 0.001)
     for i,_ in enumerate(axis):
@@ -92,11 +60,53 @@ def axis(len, pos, scale, rounding):
     return axis
 
 
+#==========================IMAGE_PRINTER==========================
+#Print the Image, pos is the wavelengt of the center, scale the wavelength per detectorsize (grate specific) (nm/mm)
+def data_img_printer_full(data: dict,name: str, pos: float, scale: float):
+    if not os.path.exists(os.getcwd() + 'img'):
+        os.makedirs(os.getcwd() + 'img')
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    im = ax.imshow(normalize(data[name]), interpolation='nearest')
+    ax.set_xticks(np.arange(len(data[name][0]))[::140] ,labels = axis(len(data[name][0]),pos,scale,True)[::140])
+    ax.set_title(name)
+    fig.tight_layout()
+    fig.savefig('img/' + name + '_full_raw.png')
+
+#Cuts lines from the image top is the first line to print, bottom the last
+def data_img_printer(data: dict,name: str,top:int, bottom:int, pos: float,scale: float):
+    if not os.path.exists(os.getcwd() + 'img'):
+        os.makedirs(os.getcwd() + 'img')
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    im = ax.imshow(normalize(data[name][top:bottom]), interpolation='nearest')
+    ax.set_xticks(np.arange(len(data[name][0]))[::140] ,labels=axis(len(data[name][0]),pos,scale,True)[::140])
+    ax.set_title(name)
+    fig.tight_layout()
+    fig.savefig('img/' + name + '_'+ str(top) + '_' + str(bottom) + '_raw.png')
+
+
+
+       
+
+        
+def histogram(data: dict,name: str,top:int, bottom:int, pos: float,scale: float):
+    if not os.path.exists(os.getcwd() + 'hist'):
+        os.makedirs(os.getcwd() + 'hist')
+    vals = norm(compress(data,name,top,bottom),0.1)
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    ax.plot(axis(len(vals),pos,scale,False),vals)
+    ax.set_title(name)
+    fig.tight_layout()
+    fig.savefig('hist/' + name + '_'+ str(top) + '_' + str(bottom) +'_hist.png')
+    plt.close()
+       
+
+
+
 def gauss(x, A, x0, sigma): 
     return A/(sigma*(np.sqrt(2*np.pi))) * (np.exp(-(x - x0) ** 2 / (2 * sigma ** 2)))
 
 # Find Peaks peakhight: multiplyer of average to count as peak, peakrange: int of numbers arround a peak that is ignored from peak search, fitrange: int range where scipy tries to fit the gaussian
-def peak_finder(data, name, top, bottom, pos, scale, plot, peakhight, peakrange, fitrange,helper):
+def peak_finder(data: dict,name: str,top:int, bottom:int, pos: float,scale: float, plot:bool, peakhight: float, peakrange: int, fitrange: int,helper: bool):
     vals = norm(compress(data,name,top,bottom),0.1)
     average = np.average(vals)
     possible_peaks = []
